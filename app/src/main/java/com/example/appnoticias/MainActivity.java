44fa50c765e7abaf16ac455643d01ffbcf1b6f58
Paulo-Galego <paulosilvanogalego@gmail.com>
2023-09-08 09:47:15 -0300

    firs upload

---
+++ app/src/main/java/com/example/appnoticias/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.appnoticias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
